6fdef8440a2b48e86fa42581e9c03c5de734b405
Azimjon Abdujalilov <azimjon.abdujalilov95@gmail.com>
2025-03-18 13:44:46 +0500

    .

--- left_join.sql
+++ left_join.sql
@@ -2,8 +2,8 @@
 SELECT employees.name, departments.department_name
 FROM employees
 CROSS JOIN departments
-ON employees.id = departments.id
+ON employees.id = departments.id_employee
 UNION
 SELECT employees.name, NULL
 FROM employees
-WHERE employees.id NOT IN (SELECT id FROM departments);
+WHERE employees.id NOT IN (SELECT id_employee FROM departments);
